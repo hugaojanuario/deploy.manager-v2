ced91f0fa6bfdd7ec96d37a2793475a5934e6d01
hugaojanuario <hugo.santariosi@siplan.com.br>
2026-03-31 12:33:26 -0300

    feat: client = entity and dtos

--- src/main/java/com/hugaojanuario/deploy/manager/domain/version/Version.java
+++ src/main/java/com/hugaojanuario/deploy/manager/domain/version/Version.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -19,6 +22,13 @@ public class Version {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    private LocalDate dateRelease;
     private String numberVersion;
     private String changelog;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    private boolean activate;
 }
