8a9df3330c4e9738b6a4c5af8d72e453d6933d22
hugaojanuario <hugo.santariosi@siplan.com.br>
2026-04-02 16:31:14 -0300

    fix: liberando os endpoints do swagger open-api

--- src/main/java/com/hugaojanuario/deploy/manager/infra/security/SecurityConfigurations.java
+++ src/main/java/com/hugaojanuario/deploy/manager/infra/security/SecurityConfigurations.java
@@ -29,6 +29,10 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers("/webjars/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
