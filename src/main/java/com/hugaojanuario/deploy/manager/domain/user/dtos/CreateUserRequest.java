56786143c27bc5cfe624345760577b1c04c109c1
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-04-03 11:17:29 -0300

    feat: email customizado para /auth/register

--- src/main/java/com/hugaojanuario/deploy/manager/domain/user/dtos/CreateUserRequest.java
+++ src/main/java/com/hugaojanuario/deploy/manager/domain/user/dtos/CreateUserRequest.java
@@ -6,8 +6,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record CreateUserRequest(@Email @Pattern(
                                         regexp = "^[a-zA-Z0-9._%+\\-]+@siplan\\.com\\.br$",
-                                        message = "O email deve ser do domínio @siplan.com.br"
-                                )
+                                        message = "O email deve ser do domínio @siplan.com.br")
                                 String email,
                                 String password,
                                 UserType userType) {
